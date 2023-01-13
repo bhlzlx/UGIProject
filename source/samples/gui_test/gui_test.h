a8fb5a7e7b7336f83b93e6f705f0f0b92167a5e9
kusugawa <bhlzlx@sina.com>
2023-01-13 19:34:03 +0800

    update

---
+++ source/samples/gui_test/gui_test.h
@@ -0,0 +1,72 @@
+﻿#include <UGIApplication.h>
+#include <cstdio>
+#include <algorithm>
+#include <ugi/ugi_declare.h>
+#include <ugi/ugi_types.h>
+#include <ugi/descriptor_binder.h>
+#include <io/archive.h>
+
+namespace ugi {
+
+    class Render {
+    private:
+        ugi::GraphicsPipeline*          _pipeline;
+        MeshBufferAllocator*            _bufferAllocator;
+        UniformAllocator*               _uniformAllocator;
+        Device*                         _device;
+        res_descriptor_t                _uboptor;                                       // matrices
+        res_descriptor_t                _texptor;
+        res_descriptor_t                _samptor;
+    public:
+        Render(Device* device, ugi::GraphicsPipeline* pipeline, ugi::MeshBufferAllocator* msalloc, UniformAllocator* uniformAllocator)
+            : _pipeline(pipeline)
+            , _bufferAllocator(msalloc)
+            , _uniformAllocator(uniformAllocator)
+            , _device(device)
+        {
+        }
+
+        ugi::Material* createMaterial(std::vector<std::string>const & params);
+
+        ugi::Renderable* createRenderable(uint8_t const* vd, uint32_t vdsize, uint16_t const* id, uint32_t indexCount, GPUAsyncLoadManager* asyncLoadManager);
+
+        void setUBO(Renderable* renderable, uint8_t* data);
+        void setSampler(Renderable* renderable, sampler_state_t sampler);
+        void setTexture(Renderable* renderable, image_view_t imageView);
+        void setRasterization(raster_state_t rasterState);
+        void bind(RenderCommandEncoder* encoder);
+        void draw(RenderCommandEncoder* enc, Renderable* renderable);
+
+        bool initialize();
+
+        void tick();
+    };
+
+    class HelloWorld : public UGIApplication {
+    private:
+        void*                           _hwnd;                                             //
+        StandardRenderContext*          _renderContext; //
+        Render*                         _render;
+        // sample resources
+        ugi::Texture*                   _textures[2];
+        ugi::image_view_t               _imageViews[2];
+        ugi::sampler_state_t            _samplerState;                                     //
+        // a renderable object
+        ugi::Renderable*                _renderable; 
+        //
+        uint32_t                        _flightIndex;                                      // flight index
+        //
+        float                           _width;
+        float                           _height;
+    public:
+        virtual bool initialize(void* _wnd, comm::IArchive* arch);
+        virtual void resize( uint32_t _width, uint32_t _height );
+        virtual void release();
+        virtual void tick();
+        virtual const char * title();
+        virtual uint32_t rendererType();
+    };
+
+}
+
+UGIApplication* GetApplication();
